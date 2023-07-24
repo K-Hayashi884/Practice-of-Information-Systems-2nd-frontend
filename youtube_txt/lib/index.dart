c0c2b908e72f6de53d9ba3d77a2c062527f36e5f
yu0ki <49509576+yu0ki@users.noreply.github.com>
2023-07-24 18:18:28 +0900

    時刻リンク動かず

--- youtube_txt/lib/index.dart
+++ youtube_txt/lib/index.dart
@@ -2,21 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:youtube_txt/model/video.dart';
 import 'package:youtube_txt/widget/drawer_menu.dart';
 import 'package:provider/provider.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
+
+  @override
+  State<IndexPage> createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+  late YoutubePlayerController _ytController;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  int parseHHMMSS(String time) {
+    List<String> parts = time.split(':');
+    int hours = 0;
+    int minutes;
+    int seconds;
+
+    if (parts.length == 3) {
+      // HH:MM:SS 形式の場合
+      hours = int.parse(parts[0]);
+      minutes = int.parse(parts[1]);
+      seconds = int.parse(parts[2]);
+    } else if (parts.length == 2) {
+      // MM:SS 形式の場合
+      minutes = int.parse(parts[0]);
+      seconds = int.parse(parts[1]);
+    } else {
+      throw const FormatException(
+          "Invalid time format. Supported formats: HH:MM:SS or MM:SS");
+    }
+
+    return hours * 3600 + minutes * 60 + seconds;
+  }
 
   @override
   Widget build(BuildContext context) {
     VideoNotifier videoNotifier = Provider.of<VideoNotifier>(context);
     final video_route = ModalRoute.of(context)!.settings.arguments as Video;
     final deviceWidth = MediaQuery.of(context).size.width;
-    var video = videoNotifier.videos[
-      videoNotifier.getId(
-        video_route.id
-        )
-    ];
+    var video = videoNotifier.videos[videoNotifier.getId(video_route.id)];
     debugPrint(video.indices.toString());
+
+    _ytController = YoutubePlayerController(
+        initialVideoId: video_route.id,
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+          mute: false,
+        ));
 
     final List<Widget> thumbnailItems = [
       Padding(
@@ -25,7 +64,9 @@ class IndexPage extends StatelessWidget {
             width: deviceWidth * 0.8,
             child: Center(
                 child: video.imageUrl != null
-                    ? SizedBox(width: deviceWidth * 0.9, child: Image.network(video.imageUrl))
+                    ? SizedBox(
+                        width: deviceWidth * 0.9,
+                        child: Image.network(video.imageUrl))
                     : Image.asset("images/dummy_thumbnail.png"))),
       ),
       Padding(
@@ -59,7 +100,13 @@ class IndexPage extends StatelessWidget {
             width: deviceWidth * 0.3,
             height: 35,
             child: TextButton(
-              onPressed: () {},
+              onPressed: () {
+                String? timestamp = indice["timestamp"];
+                int timeFloat = timestamp != null
+                    ? parseHHMMSS(timestamp)
+                    : 0; // Set a default time of 0 seconds if timestamp is null
+                _ytController.seekTo(Duration(seconds: timeFloat));
+              },
               child: Text(indice["timestamp"]!),
             ),
           ),
