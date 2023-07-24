dc185f506ea83fd8e4efff401f7113e4ba62dfa7
kazuki3087 <matsumoto-kazuki-25@docomo.ne.jp>
2023-07-24 20:13:51 +0900

    seek完成

--- youtube_txt/lib/index.dart
+++ youtube_txt/lib/index.dart
@@ -4,11 +4,39 @@ import 'package:youtube_txt/widget/drawer_menu.dart';
 import 'package:provider/provider.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
-class IndexPage extends StatelessWidget {
+class IndexPage extends StatefulWidget {
   const IndexPage({super.key});
 
   @override
+  State<IndexPage> createState() => _IndexPageState();
+}
+
+class _IndexPageState extends State<IndexPage> {
+
+  late YoutubePlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = YoutubePlayerController(
+      params: const YoutubePlayerParams(
+        showControls: true,
+        mute: false,
+        showFullscreenButton: false,
+        loop: false,
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.close();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    debugPrint("build index");
     VideoNotifier videoNotifier = Provider.of<VideoNotifier>(context);
     final video_route = ModalRoute.of(context)!.settings.arguments as Video;
     final deviceWidth = MediaQuery.of(context).size.width;
@@ -19,6 +47,8 @@ class IndexPage extends StatelessWidget {
     ];
     debugPrint(video.indices.toString());
 
+    _controller.cueVideoById(videoId:video_route.id); 
+
     // final _controller = YoutubePlayerController(
     //   params: YoutubePlayerParams(
     //     mute: false,
@@ -27,11 +57,11 @@ class IndexPage extends StatelessWidget {
     //   ),
     // );
 
-    final _controller = YoutubePlayerController.fromVideoId(
-      videoId: video_route.id,
-      autoPlay: false,
-      params: const YoutubePlayerParams(showFullscreenButton: true),
-    );
+    // final _controller = YoutubePlayerController.fromVideoId(
+    //   videoId: video_route.id,
+    //   autoPlay: false,
+    //   params: const YoutubePlayerParams(showFullscreenButton: true),
+    // );
 
     double parseHHMMSS(String time) {
     List<String> parts = time.split(':');
@@ -103,17 +133,16 @@ class IndexPage extends StatelessWidget {
             width: deviceWidth * 0.3,
             height: 35,
             child: TextButton(
-              onPressed: () {},
-              child: TextButton(
               onPressed: () {
                 String? timestamp = indice["timestamp"];
                 double timeFloat = timestamp != null
                     ? parseHHMMSS(timestamp)
                     : 0; // Set a default time of 0 seconds if timestamp is null
-                _controller.seekTo(seconds: timeFloat);
+                _controller.seekTo(seconds: timeFloat,allowSeekAhead:true);
+                var playerState = _controller.playerState;
+                debugPrint(playerState.hashCode.toString());
               },
               child: Text(indice["timestamp"]!),
-            ),
             ),
           ),
           SizedBox(
