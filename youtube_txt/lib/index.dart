688730ae94798e5de027a02052d6406cf8e67592
yu0ki <49509576+yu0ki@users.noreply.github.com>
2023-07-24 20:28:06 +0900

    動いたぞ！

--- youtube_txt/lib/index.dart
+++ youtube_txt/lib/index.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:youtube_txt/model/video.dart';
+import 'package:youtube_txt/requester/requester.dart';
 import 'package:youtube_txt/widget/drawer_menu.dart';
 import 'package:provider/provider.dart';
-import 'package:youtube_txt/requester/requester.dart';
 import 'package:youtube_player_iframe/youtube_player_iframe.dart';
 
 class IndexPage extends StatefulWidget {
@@ -40,117 +40,122 @@ class _IndexPageState extends State<IndexPage> {
     VideoNotifier videoNotifier = Provider.of<VideoNotifier>(context);
     final videoRoute = ModalRoute.of(context)!.settings.arguments as Video;
     final deviceWidth = MediaQuery.of(context).size.width;
-    var video = videoNotifier.videos[videoNotifier.getId(video_route.id)];
+    var video = videoRoute;
+    if (videoNotifier.getId(videoRoute.id) != -1) {
+      video = videoNotifier.videos[videoNotifier.getId(videoRoute.id)];
+    }
     debugPrint(video.indices.toString());
 
-    _controller.cueVideoById(videoId: video_route.id);
+    _controller.cueVideoById(videoId: videoRoute.id);
 
-      // final _controller = YoutubePlayerController(
-      //   params: YoutubePlayerParams(
-      //     mute: false,
-      //     showControls: true,
-      //     showFullscreenButton: false,
-      //   ),
-      // );
+    // final _controller = YoutubePlayerController(
+    //   params: YoutubePlayerParams(
+    //     mute: false,
+    //     showControls: true,
+    //     showFullscreenButton: false,
+    //   ),
+    // );
 
     // final _controller = YoutubePlayerController.fromVideoId(
-    //   videoId: video_route.id,
+    //   videoId: videoRoute.id,
     //   autoPlay: false,
     //   params: const YoutubePlayerParams(showFullscreenButton: true),
     // );
 
-      double parseHHMMSS(String time) {
-        List<String> parts = time.split(':');
-        int hours = 0;
-        int minutes;
-        int seconds;
+    double parseHHMMSS(String time) {
+      List<String> parts = time.split(':');
+      int hours = 0;
+      int minutes;
+      int seconds;
 
-        if (parts.length == 3) {
-          // HH:MM:SS 形式の場合
-          hours = int.parse(parts[0]);
-          minutes = int.parse(parts[1]);
-          seconds = int.parse(parts[2]);
-        } else if (parts.length == 2) {
-          // MM:SS 形式の場合
-          minutes = int.parse(parts[0]);
-          seconds = int.parse(parts[1]);
-        } else {
-          throw const FormatException(
-              "Invalid time format. Supported formats: HH:MM:SS or MM:SS");
-        }
-
-        return (hours * 3600 + minutes * 60 + seconds).toDouble();
+      if (parts.length == 3) {
+        // HH:MM:SS 形式の場合
+        hours = int.parse(parts[0]);
+        minutes = int.parse(parts[1]);
+        seconds = int.parse(parts[2]);
+      } else if (parts.length == 2) {
+        // MM:SS 形式の場合
+        minutes = int.parse(parts[0]);
+        seconds = int.parse(parts[1]);
+      } else {
+        throw const FormatException(
+            "Invalid time format. Supported formats: HH:MM:SS or MM:SS");
       }
 
-      final List<Widget> thumbnailItems = [
-        Padding(
-          padding: const EdgeInsets.only(top: 16.0),
-          child: SizedBox(
+      return (hours * 3600 + minutes * 60 + seconds).toDouble();
+    }
+
+    final List<Widget> thumbnailItems = [
+      Padding(
+        padding: const EdgeInsets.only(top: 16.0),
+        child: SizedBox(
             width: deviceWidth * 0.8,
             child: Center(
-              child: YoutubePlayer(
-                controller: _controller,
-                aspectRatio: 16 / 9,
-              ),
-            ),
+                child: YoutubePlayer(
+              controller: _controller,
+              aspectRatio: 16 / 9,
+            )
+                // child: video.imageUrl != null
+                //     ? SizedBox(width: deviceWidth * 0.9, child: Image.network(video.imageUrl))
+                //     : Image.asset("images/dummy_thumbnail.png")
+                )),
+      ),
+      Padding(
+        padding: const EdgeInsets.only(bottom: 8.0),
+        child: SizedBox(
+          width: deviceWidth * 0.9,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              SizedBox(
+                  width: deviceWidth * 0.4,
+                  child: Text(
+                    video.title,
+                    style: const TextStyle(
+                        fontSize: 17, fontWeight: FontWeight.w500),
+                    overflow: TextOverflow.ellipsis,
+                  )),
+              OutlinedButton(
+                  onPressed: () {
+                    Requester().laterAddRequester(videoRoute.id).then((_) {
+                      showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            title: const Text("追加しました"),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    }).onError((error, stackTrace) {
+                      showDialog(
+                        context: context,
+                        builder: (_) {
+                          return AlertDialog(
+                            title: Text(error.toString()),
+                            actions: [
+                              TextButton(
+                                onPressed: () => Navigator.pop(context),
+                                child: const Text("OK"),
+                              ),
+                            ],
+                          );
+                        },
+                      );
+                    });
+                  },
+                  child: const Text("あとで見る")),
+            ],
           ),
         ),
-        Padding(
-          padding: const EdgeInsets.only(bottom: 8.0),
-          child: SizedBox(
-            width: deviceWidth * 0.9,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                SizedBox(
-                    width: deviceWidth * 0.4,
-                    child: Text(
-                      video.title,
-                      style: const TextStyle(
-                          fontSize: 17, fontWeight: FontWeight.w500),
-                      overflow: TextOverflow.ellipsis,
-                    )),
-                OutlinedButton(
-                    onPressed: () {
-                      Requester().laterAddRequester(videoRoute.id).then((_) {
-                        showDialog(
-                          context: context,
-                          builder: (_) {
-                            return AlertDialog(
-                              title: const Text("追加しました"),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text("OK"),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      }).onError((error, stackTrace) {
-                        showDialog(
-                          context: context,
-                          builder: (_) {
-                            return AlertDialog(
-                              title: Text(error.toString()),
-                              actions: [
-                                TextButton(
-                                  onPressed: () => Navigator.pop(context),
-                                  child: const Text("OK"),
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      });
-                    },
-                    child: const Text("あとで見る")),
-              ],
-            ),
-          ),
-        ),
-      ];
+      ),
+    ];
 
     final List<Widget> indicesList = [];
     if (video.indices != null) {
@@ -186,65 +191,49 @@ class _IndexPageState extends State<IndexPage> {
       ));
     }
 
-      final List<Widget> commentsList = [];
-      if (video.comments != null) {
-        commentsList.add(
-          Container(
-            width: deviceWidth,
-            alignment: Alignment.center,
-            child: Container(
+    final List<Widget> commentsList = [];
+    if (video.comments != null) {
+      commentsList.add(
+        Container(
+          width: deviceWidth,
+          alignment: Alignment.center,
+          child: Container(
               padding: const EdgeInsets.only(bottom: 12),
               width: deviceWidth * 0.9,
-              child: const Text("コメントからのおすすめ"),
-            ),
-          ),
-        );
-        for (String comment in video.comments!) {
-          commentsList.add(
-            Container(
-              padding: const EdgeInsets.only(bottom: 12),
-              width: deviceWidth,
-              alignment: Alignment.center,
-              child: Flexible(
-                child: SizedBox(
-                  width: deviceWidth * 0.8,
-                  child: Text(
-                    comment,
-                    textAlign: TextAlign.start,
-                  ),
-                ),
-              ),
-            ),
-          );
-        }
-        commentsList.add(const SizedBox(
-          height: 12,
-        ));
-      }
-
-      return Scaffold(
-        appBar: AppBar(title: const Text("Youtube.txt")),
-        endDrawer: const DrawerMenu(),
-        body: CustomScrollView(
-          slivers: [
-            SliverList(delegate: SliverChildListDelegate(thumbnailItems)),
-            SliverList(delegate: SliverChildListDelegate(indicesList)),
-            SliverList(delegate: SliverChildListDelegate(commentsList)),
-          ],
+              child: const Text("コメントからのおすすめ")),
         ),
       );
-    } catch (e) {
-      // エラーが発生した場合は"later"への画面遷移を返す
-      return FutureBuilder(
-        future: Navigator.pushReplacementNamed(context, "later"),
-        builder: (context, snapshot) {
-          return const Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
+      for (String comment in video.comments!) {
+        commentsList.add(Container(
+          padding: const EdgeInsets.only(bottom: 12),
+          width: deviceWidth,
+          alignment: Alignment.center,
+          child: Flexible(
+            child: SizedBox(
+              width: deviceWidth * 0.8,
+              child: Text(
+                comment,
+                textAlign: TextAlign.start,
+              ),
             ),
-          );
-        },
-      );
+          ),
+        ));
+      }
+      commentsList.add(const SizedBox(
+        height: 12,
+      ));
     }
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Youtube.txt")),
+      endDrawer: const DrawerMenu(),
+      body: CustomScrollView(
+        slivers: [
+          SliverList(delegate: SliverChildListDelegate(thumbnailItems)),
+          SliverList(delegate: SliverChildListDelegate(indicesList)),
+          SliverList(delegate: SliverChildListDelegate(commentsList))
+        ],
+      ),
+    );
   }
 }
