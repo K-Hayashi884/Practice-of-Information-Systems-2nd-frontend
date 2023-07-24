3db9bbed35dc5e3433a911bb6427e79ef29c4984
K-Hayashi884 <71001904+K-Hayashi884@users.noreply.github.com>
2023-07-25 03:37:22 +0900

    Revert "Firebase login"

--- youtube_txt/lib/main.dart
+++ youtube_txt/lib/main.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:youtube_txt/auth_provider.dart';
-
-import 'package:youtube_txt/Userpage.dart';
 import 'package:youtube_txt/top.dart';
 
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -25,9 +12,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.white,
       ),
       routes: {
-        '/top': (context) => const TopPage(),
-        // 'index': (context) => const IndexPage(),
-        "/": (context) => const UserPage(),
+        '/': (context) => TopPage(),
       },
     );
   }
