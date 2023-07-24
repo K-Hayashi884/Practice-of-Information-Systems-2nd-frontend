9b3ec5280cfdcae686e7aef11af2fcd8b8cc5cd1
K-Hayashi884 <71001904+K-Hayashi884@users.noreply.github.com>
2023-07-25 04:33:08 +0900

    Revert "Revert "Firebase login""

---
+++ youtube_txt/lib/Userpage.dart
@@ -0,0 +1,269 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:youtube_txt/top.dart';
+import 'package:youtube_txt/urls.dart';
+import 'package:http/http.dart' as http;
+
+class UserPage extends StatefulWidget {
+  const UserPage({Key? key}) : super(key: key);
+
+  @override
+  _UserPageState createState() => _UserPageState();
+}
+
+class _UserPageState extends State<UserPage> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Youtube.txt'),
+            bottom: const TabBar(
+              tabs: [
+                Tab(text: 'Login'),
+                Tab(text: 'SignUp'),
+              ],
+            ),
+          ),
+          body: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 120, horizontal: 50),
+            child: TabBarView(
+              children: [
+                LoginPage(),
+                SignupPage(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String _email = "", _password = "", type = "login";
+  User? user;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final loginEmailFromui = TextEditingController();
+    final loginPasswordFromui = TextEditingController();
+    String errorText = "";
+
+    return Column(children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        child: const Text('Login',
+            style: TextStyle(fontSize: 30, color: Colors.black)),
+      ),
+      const SizedBox(height: 20),
+      TextField(
+        controller: loginEmailFromui,
+        decoration: const InputDecoration(
+          labelText: "メールアドレス：",
+        ),
+      ),
+      const SizedBox(height: 10),
+      TextField(
+          controller: loginPasswordFromui,
+          decoration: const InputDecoration(labelText: "パスワード："),
+          obscureText: true),
+      const SizedBox(height: 20),
+      // 失敗したらエラーを表示
+      if (errorText.isNotEmpty)
+        Text(
+          errorText,
+          style: const TextStyle(color: Colors.red),
+        ),
+      SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+              onPressed: () {
+                _email = loginEmailFromui.text;
+                _password = loginPasswordFromui.text;
+                _login(errorText); //loginのための関数
+              },
+              child: const Text('Login')))
+    ]);
+  }
+
+  Future<void> _login(String errorText) async {
+    try {
+      final User? user =
+          (await FirebaseAuth.instance.signInWithEmailAndPassword(
+        email: _email,
+        password: _password,
+      ))
+              .user;
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('${user!.email} signed in'),
+        ),
+      );
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const TopPage()),
+      );
+    } catch (e) {
+      print(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        // const SnackBar(
+        //   content: Text('Failed to sign in with Email & Password'),
+        // ),
+        SnackBar(
+          content: Text(e.toString()),
+        ),
+      );
+    }
+  }
+}
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({Key? key}) : super(key: key);
+
+  @override
+  _SignupPageState createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  String _email = "",
+      _username = "",
+      _password = "",
+      _passwordAgain = "",
+      type = "login";
+  User? user;
+
+  @override
+  Widget build(BuildContext context) {
+    final signupEmailFromui = TextEditingController();
+    final signupUsernameFromui = TextEditingController();
+    final signupPasswordFromui = TextEditingController();
+    final signupPasswordAgainFromui = TextEditingController();
+    String _errorText = '';
+
+    return SingleChildScrollView(
+        child: Column(children: [
+      Container(
+        alignment: Alignment.centerLeft,
+        child: const Text('Sign Up',
+            style: TextStyle(fontSize: 30, color: Colors.black)),
+      ),
+      const SizedBox(height: 20),
+      TextField(
+        controller: signupEmailFromui,
+        decoration: const InputDecoration(
+          labelText: "メールアドレス：",
+        ),
+      ),
+      const SizedBox(height: 20),
+      TextField(
+        controller: signupUsernameFromui,
+        decoration: const InputDecoration(
+          labelText: "ユーザ名：",
+        ),
+      ),
+      const SizedBox(height: 10),
+      TextField(
+          controller: signupPasswordFromui,
+          decoration: const InputDecoration(labelText: "パスワード："),
+          obscureText: true),
+      const SizedBox(height: 10),
+      TextField(
+          controller: signupPasswordAgainFromui,
+          decoration: const InputDecoration(labelText: "パスワード（確認）："),
+          obscureText: true),
+      const SizedBox(height: 20),
+      // 失敗したらエラーを表示
+      if (_errorText.isNotEmpty)
+        Text(
+          _errorText,
+          style: const TextStyle(color: Colors.red),
+        ),
+      SizedBox(
+          width: double.infinity,
+          child: ElevatedButton(
+              onPressed: () {
+                _email = signupEmailFromui.text;
+                _username = signupUsernameFromui.text;
+                _password = signupPasswordFromui.text;
+                _passwordAgain = signupPasswordAgainFromui.text;
+                // パスワードが同じか確認
+                if (_password == _passwordAgain) {
+                  _signup(); //signupのための関数
+                  // _emailをDjangoに送信する
+                  _sendUserInfo();
+                } else {
+                  setState(() {
+                    _errorText = 'パスワードが一致しません';
+                  });
+                  print(_errorText);
+                }
+              },
+              child: const Text('Sign Up')))
+    ]));
+  }
+
+  Future<void> _signup() async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _email,
+        password: _password,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('${user!.email} signed in'),
+        ),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> _sendUserInfo() async {
+    final response = await http.post(
+      SignUpUri(),
+      body: {'email': _email, "username": _username},
+    );
+    print(response);
+    if (response.statusCode == 201) {
+      // サインアップ成功時
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const TopPage()),
+      );
+    } else {
+      // リクエスト失敗の処理
+      print(response.body);
+      throw Exception(
+          'Request failed with status ${response.statusCode}' + response.body);
+    }
+  }
+}
--- youtube_txt/lib/top.dart
+++ youtube_txt/lib/top.dart
@@ -1,7 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:youtube_txt/urls.dart';
+import 'Userpage.dart';
+import 'package:youtube_txt/urls.dart';
 
 class TopPage extends StatefulWidget {
-  const TopPage({super.key});
+  const TopPage({Key? key});
 
   @override
   State<TopPage> createState() => _TopPageState();
@@ -30,25 +35,72 @@ class _TopPageState extends State<TopPage> {
                 // },
               ),
             ),
-            VideoList([
-              VideoListTile(
-                  Image.asset("images/dummy_thumbnail.png"), "おすすめ動画１"),
-              VideoListTile(
-                  Image.asset("images/dummy_thumbnail.png"), "おすすめ動画２"),
-              VideoListTile(
-                  Image.asset("images/dummy_thumbnail.png"), "おすすめ動画３"),
-            ])
+            FutureBuilder<void>(
+              future: _checkLoginAndMakeRequest(context),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  // ログインチェックとリクエストが完了するまでローディング表示
+                  return CircularProgressIndicator();
+                } else if (snapshot.hasError) {
+                  // エラーハンドリング
+                  return Text('Error: ${snapshot.error}');
+                } else {
+                  // リクエストが成功した場合の表示
+                  return VideoList([
+                    VideoListTile(
+                        Image.asset("images/dummy_thumbnail.png"), "おすすめ動画１"),
+                    VideoListTile(
+                        Image.asset("images/dummy_thumbnail.png"), "おすすめ動画２"),
+                    VideoListTile(
+                        Image.asset("images/dummy_thumbnail.png"), "おすすめ動画３"),
+                  ]);
+                }
+              },
+            ),
           ],
         ),
       ),
     );
+  }
+
+  Future<void> _checkLoginAndMakeRequest(BuildContext context) async {
+    // ログイン済みかどうかチェック
+    String _idToken = "";
+    User? currentUser = FirebaseAuth.instance.currentUser;
+
+    if (currentUser != null) {
+      // ログインしている場合
+      _idToken = await currentUser.getIdToken();
+      print("_idToken: " + _idToken);
+      http.Response response = await http.get(
+        TopUri(),
+        headers: {'Authorization': 'Bearer $_idToken'},
+      );
+
+      if (response.statusCode == 200) {
+        // リクエスト成功の処理
+        print(response.body);
+      } else {
+        // リクエスト失敗の処理
+        print(response.body);
+        throw Exception('Request failed with status ${response.statusCode}' +
+            response.body);
+      }
+    } else {
+      // ログインしていない場合
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const UserPage()),
+      );
+      // throw Exception('User is not logged in');
+    }
   }
 }
 
 class VideoList extends StatelessWidget {
   final List<VideoListTile> videos;
 
-  const VideoList(this.videos, {super.key});
+  const VideoList(this.videos, {Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -65,7 +117,7 @@ class VideoList extends StatelessWidget {
       itemCount: videos.length,
       scrollDirection: Axis.vertical,
       shrinkWrap: true,
-      physics: NeverScrollableScrollPhysics(),
+      physics: const NeverScrollableScrollPhysics(),
     );
   }
 }
@@ -74,7 +126,7 @@ class VideoListTile extends StatelessWidget {
   final Image image;
   final String title;
 
-  const VideoListTile(this.image, this.title, {super.key});
+  const VideoListTile(this.image, this.title, {Key? key});
 
   @override
   Widget build(BuildContext context) {
